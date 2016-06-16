f4609d03dba1afc258cc69b1de4f376b75399312
swaption2009 <swaption2009@gmail.com>
2016-06-16 16:01:33 -0700

    refactor prepare_brand_data method

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,9 @@
 require 'json'
 
-# FORMULA
+# FORMULAS
 
 def average_price(sales, quantities)
-  (sales / quantities).round(2)
+  sales / quantities
 end
 
 def total_sales(sales)
@@ -15,39 +15,37 @@ end
 
 def print_product_report_data
   $products_hash["items"].each do |product|
-  # Print the name of the toy
-  puts "Product name: " + product["title"]
-  # Print the retail price of the toy
-  puts "Retail price: $" + product["full-price"]
-  # Calculate and print the total number of purchases
-  puts "Total number of purchases: " + product["purchases"].count.to_s
-  # Calculate and print the total amount of sales
-  sales = []
-  product["purchases"].each do |price|
-    sales << price["price"]
-  end
-  puts "Total amount of sales: $" + total_sales(sales).to_s
+    puts "Product name: " + product["title"]
+    puts "Retail price: $" + product["full-price"]
+    puts "Total number of purchases: " + product["purchases"].count.to_s
 
-  # Calculate and print the average price the toy sold for
-  puts "Average price is $" + average_price(total_sales(sales), product["purchases"].count).to_s
-
+    sales = []
+      product["purchases"].each do |price|
+        sales << price["price"]
+      end
+    puts "Total amount of sales: $" + total_sales(sales).to_s
+    puts "Average price is $" + average_price(total_sales(sales), product["purchases"].count).to_s
   line_divider
   end
 end
 
-def print_brand_report_data
-  brands = []
+def prepare_brand_data
+  $brands = []
   $products_hash["items"].each do |product|
-    brands << product["brand"] unless brands.include? product["brand"]
+    $brands << product["brand"] unless $brands.include?(product["brand"])
   end
-  puts "There are " + brands.count.to_s + " brands. They are: " + brands.to_s
+end
+
+def print_brand_report_data
+  prepare_brand_data
+  puts "There are " + $brands.count.to_s + " brands. They are: " + $brands.to_s
 
   line_divider
 
-  brands.each do |brand|
+  $brands.each do |brand|
     brand_stock = 0
     brand_price = 0
-    brand_revenue = 0
+    brand_revenue = []
     brand_qty_sold = 0
     brand_item_counter = 0
 
@@ -57,19 +55,16 @@ def print_brand_report_data
         brand_stock += product["stock"]
         brand_price += product["full-price"].to_f
         product["purchases"].each do |purchase|
-          brand_qty_sold += 1
-          brand_revenue += purchase["price"]
+          brand_revenue << purchase["price"]
         end
       end
     end
-    # Count and print the number of the brand's toys we stock
-    puts brand.to_s + " has " + brand_stock.to_s + " quantities in stock"
-    # Calculate and print the average price of the brand's toys
-    puts "The average price for " + brand + " brand is: $" + average_price(brand_price, brand_item_counter).to_s
-    # Calculate and print the total revenue of all the brand's toy sales combined
-    puts "The store has sold " + brand + " brand for a total of: $" + brand_revenue.round(2).to_s
 
-    line_divider
+    puts brand.to_s + " has " + brand_stock.to_s + " quantities in stock"
+    puts "The average price for " + brand + " brand is: $" + average_price(brand_price, brand_item_counter).round(2).to_s
+    puts "The store has sold " + brand + " brand for a total of: $" + total_sales(brand_revenue).round(2).to_s
+
+  line_divider
   end
 end
 
